81e5b7c12c8c085a986a3be3583e2a84ded8a4b1
Jianwei Xu <forestserenity@gmail.com>
2017-10-23 08:42:54 +0800

    render

--- src/utils/solr_util.py
+++ src/utils/solr_util.py
@@ -66,7 +66,89 @@ def solr_qa(core, query):
     return docs
 
 
+def solr_facet(mappers, facet_field, is_range):
+
+    def render_range(a, gap):
+        if len(a) == 0:
+            return []
+        components = []
+        p = 0
+        if len(a) == 1:
+            components.append(str(a))
+        elif len(a) == 2:
+            components.append(str(a[0]))
+            components.append(str(a[1]))
+        else:
+            for i in range(1, len(a)):
+                if a[i] - a[i - 1] > gap:
+                    if i - p == 1:
+                        components.append(str(a[p]))
+                    else:
+                        components.append(str(a[p]) + "-" + str(a[i - 1]))
+                    p = i
+            if len(components) == 0:
+                components.append(str(a[1]) + "-" + str(a[-1]))
+        render = []
+        if len(components) == 1:
+            render.append(components[0])
+        else:
+            for i in range(0, len(components) - 1):
+                if '-' in components[i + 1]:
+                    render.append(components[i])
+                    continue
+                if '-' in components[i]:
+                    render.append(components[i])
+                    continue
+                render.append(components[i] + "-" + components[i + 1])
+        return render
+
+    if not is_range:
+        params = {
+            'q': '*:*',
+            'facet': True,
+            'facet.field': facet_field,
+            "facet.mincount": 1
+        }
+        params['fq'] = compose_fq(mappers)
+        res = solr.query('category', params)
+        facets = res.get_facet_keys_as_list(facet_field)
+        return res.get_facet_keys_as_list(facet_field), len(facets)
+    else:
+        start = 1
+        gap = 1
+        end = 100
+        # use facet.range
+        if facet_field == "price":
+            start = 100
+            gap = 3000
+            end = 30000
+        if facet_field == 'ac.power_float':
+            start = 1
+            gap = 0.5
+            end = 10
+        params = {
+            'q': '*:*',
+            'facet': True,
+            'facet.range': facet_field,
+            "facet.mincount": 1,
+            'facet.range.start': start,
+            'facet.range.end': end,
+            'facet.range.gap': gap
+        }
+        params['fq'] = compose_fq(mappers)
+        res = solr.query('category', params)
+        ranges = res.get_facets_ranges()[facet_field].keys()
+        ranges = [float("{0:.1f}".format(float(r))) for r in ranges]
+        # now render the result
+        facet = render_range(ranges, gap)
+        return facet, len(ranges)
+
+
 if __name__ == "__main__":
     mapper = {"category":"a", "brand":"b"}
     options = ["price"]
     print(compose_fq(mapper, options))
+
+    mappers = {'brand':"苹果","category":"手机"}
+    facet_field = 'location'
+    print(solr_facet(mappers=mappers, facet_field=facet_field, is_range=False))
