21c9250588eb44b2fdc02bfeab18ed7f3763479f
cb <cbandhy@gmail.com>
2017-10-25 13:59:06 +0800

    fix update.py

--- src/preprocess/update.py
+++ src/preprocess/update.py
@@ -11,7 +11,8 @@ grandfatherdir = os.path.dirname(os.path.dirname(
 sys.path.insert(0, parentdir)
 import memory.config as config
 
-extra_path = os.path.join(grandfatherdir, 'data/memn2n/train/tree/extra/data')
+extra_path = os.path.join(
+    grandfatherdir, 'data/memn2n/train/tree/extra/data_test')
 train_path = os.path.join(
     grandfatherdir, 'data/memn2n/train/tree/origin/train.txt')
 new_train_path = os.path.join(
@@ -20,6 +21,64 @@ candidate_path = os.path.join(
     grandfatherdir, 'data/memn2n/train/tree/origin/candidates.txt')
 new_candidate_path = os.path.join(
     grandfatherdir, 'data/memn2n/train/tree/candidates.txt')
+
+
+def update2():
+    candidates = set()
+    with open(candidate_path, 'r') as f:
+        for line in f:
+            line = line.strip().lower()
+            if len(line):
+                if not line.startswith('reserved_'):
+                    candidates.add(line)
+
+    with open(extra_path, 'r') as f:
+        extra_data = f.readlines()
+    # extra_data=[a.strip('\n').lower() for a in extra_data]
+    print(extra_data)
+    extra = []
+    tmp = []
+    for line in extra_data:
+        if line == '\n':
+            extra.append(tmp)
+            tmp = []
+        else:
+            line = line.strip().lower()
+            tmp.append(line)
+    extra.append(tmp)
+    print(extra)
+    extra_data = []
+    for dialog in extra:
+        if len(dialog) and len(candidates) < config.CANDIDATE_POOL:
+            candids = [line.split('\t')[1] for line in dialog]
+            num = sum([c in candidates for c in candids])
+            if len(candidates) + num <= config.CANDIDATE_POOL:
+                for c in candids:
+                    candidates.add(c)
+                extra_data.append(dialog)
+            else:
+                continue
+    print(extra_data)
+    candidates = list(candidates)
+    candidates.sort()
+    print('new candidates num', len(candidates))
+    len_origin = len(candidates)
+    if len_origin < config.CANDIDATE_POOL:
+        for i in range(config.CANDIDATE_POOL - len_origin):
+            candidates.append('reserved_' + str(i + len_origin))
+
+    with open(new_train_path, 'w') as f:
+        with open(train_path, 'r') as tf:
+            for line in tf:
+                f.writelines(line)
+            f.writelines('\n')
+        for dialog in extra_data:
+            for l in dialog:
+                f.write(l + '\n')
+            f.write('\n')
+    with open(new_candidate_path, 'w') as f:
+        for l in candidates:
+            f.write(l + '\n')
 
 
 def update():
@@ -72,7 +131,7 @@ def update():
 
 
 def main():
-    update()
+    update2()
 
 
 if __name__ == '__main__':
