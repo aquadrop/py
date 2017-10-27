d97134c23959e129f984677ca326636b1f663b1c
Jianwei Xu <forestserenity@gmail.com>
2017-10-27 10:18:49 +0800

    ambiguity fix

--- src/kernel/belief_tracker.py
+++ src/kernel/belief_tracker.py
@@ -308,6 +308,14 @@ class BeliefTracker:
             if self.AMBIGUITY_PICK in self.requested_slots and flag:
                 self.requested_slots.remove(self.AMBIGUITY_PICK)
             # del slot_values_mapper[self.AMBIGUITY_PICK]
+        else:
+            # rule based
+            if self.AMBIGUITY_PICK in slot_values_mapper:
+                value = slot_values_mapper[self.AMBIGUITY_PICK]
+                del slot_values_mapper[self.AMBIGUITY_PICK]
+                slot_values_mapper[self.API] = value
+                values_marker[self.API] = 0
+
         # look for virtual memory
         if self.VIRTUAL in slot_values_mapper:
             self.machine_state = self.API_REQUEST_STATE
