9819c43f75b75b22855f907c6f1694b787bb0c50
Jianwei Xu <forestserenity@gmail.com>
2017-11-23 15:05:18 +0800

    fix json of numpy float

--- src/web_2.py
+++ src/web_2.py
@@ -89,7 +89,6 @@ def chat():
                     return json.dumps(result, ensure_ascii=False)
             u_i_kernel = lru_kernels[u]
             result = u_i_kernel.kernel(q=q, user=u)
-            print(result)
             result = {"question": q, "sentence": q,"result": result, "user": u}
             return json.dumps(result, ensure_ascii=False)
 
