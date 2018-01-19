a3991a1d6de40d79094284071e5b15abb946c964
xzy <506676702@qq.com>
2018-01-19 10:18:16 +0800

    file-modeltransform improve

--- src/utils/model_transform.py
+++ src/utils/model_transform.py
@@ -5,13 +5,16 @@ import re
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 grandfatherdir = os.path.dirname(parentdir)
 sys.path.insert(0, parentdir)
-print(grandfatherdir)
 print(parentdir)
-from utils.mongodb_client import Mongo
+# from utils.mongodb_client import Mongo
 
-
+from mongodb_client import Mongo
 class Model_trans(Mongo):
-
+    '''
+        Tansform the model files in dir(renser_2) to data format for mangdb.
+        model_category: make the model category optional, such as media.  
+        f_path： the abspath of given model file.
+    '''
     def __init__(self, fname, model_category = 'location', db_name= 'bookstore', ip= 'bookstore', port= '27017' ):
 
         super(Model_trans,self).__init__(db_name, ip, port)
@@ -20,66 +23,65 @@ class Model_trans(Mongo):
 
 
     def trans(self):
-        '''Tansform the model files in dir(renser_2) to data format for mangdb.
-            model_category: make the model category optional, such as media.  
-            f_path： the abspath of given model file.
-        '''
+
         try:
-            if self.model_category == 'location':
+            if self.model_category == 'render_location':
                 with open(self.f_path, 'r') as f_in:
-                    data_list = []
-                    value_list = []
-                    key_list = ['template', 'suitable', 'unsuitable']
+                    dataList = []
+                    valueList = []
+                    keyList = ['template', 'suitable', 'unsuitable']
                     for eachline in f_in.readlines():
-                        value_list = eachline.strip().split('#')
-                        key_value = tuple(zip(key_list, value_list))
-                        data_list.append(dict(key_value))
-                # return data_list
-            elif self.model_category == 'media':
-                with open(self.f_path, 'r') as f_in:
-                    data_list = []
-                    value_list = []
-                    key_list = ['instruct', 'img']
-                    for eachline in f_in.readlines():
-                        value_list = eachline.strip().split('#')
-                        key_value = tuple(zip(key_list, value_list))
-                        data_list.append(dict(key_value))
-            elif self.model_category == 'api':
-                with open(self.f_path, 'r') as f_in:
-                    data_list = []
-                    value_list = []
-                    replies_list = []
-                    key_list = ['instruct', 'replies']
-                    for eachline in f_in.readlines():
-                        sentence_list = re.split('#{2}|\|', eachline.strip())
+                        valueList = eachline.strip().split('#')
+                        key_value = tuple(zip(keyList, valueList))
+                        dataList.append(dict(key_value))
 
-                        # the value to the key 'replies' will be stored in array format
-                        # just take the ending part
-                        for eachanswer in sentence_list[2:]:
-                            replies_list.append(eachanswer)
-                        value_list.append(sentence_list[0])
-                        value_list.append(replies_list)
-                        # print(value_list)
-                        key_value = tuple(zip(key_list, value_list))
-                        data_list.append(dict(key_value))
-                        replies_list = [] #empty the list !!!
-                        value_list = []
+            elif self.model_category == 'render_api':
+                dataList = []
+                keyList = ['media', 'equal_questions', 'answers', 'emotion_url', 'group',
+                            'label', 'emotion_name', 'time_out']
+                f_media = open(os.path.join(grandfatherdir, 'model/render_2/render_media.txt'), 'r')
+                media = {}
+                for line in f_media.readlines():
+                    sentence = line.strip()
+                    if sentence: media[sentence.split('#')[0]] = sentence.split('#')[-1]
+                # print('media', media)
 
+                f_api = open(self.f_path, 'r')
+                dict_num = 0
+                for line in f_api.readlines():
+                    sentence = re.split(r'#{2}|\|', line.strip())
+                    # print (sentence)
+                    if line:
+                        dataList.append([])
+                        for key in keyList:
+                            if key == 'media': dataList[dict_num].append((key, media.get(sentence[0], 'null')))
+                            elif key == 'equal_questions': dataList[dict_num].append((key, [i for i in sentence[:1]]))
+                            elif key == 'answers': dataList[dict_num].append((key, [i for i in sentence[-1:]]))
+                            elif key == 'group': dataList[dict_num].append((key, 'render_api'))
+                            elif key == 'label': dataList[dict_num].append((key, sentence[0]))
+                            else: dataList[dict_num].append((key, 'null'))
+                        # print(dataList)
+                        dataList[dict_num] = dict(dataList[dict_num])
+                        dict_num += 1
+                f_media.close()
+                f_api.close()
+
+               
             elif self.model_category == 'synonym':
                 with open(self.f_path, 'r') as f_in:
-                    data_list = []
-                    value_list = []
-                    key_list = ['given', 'matched']
+                    dataList = []
+                    valueList = []
+                    keyList = ['given', 'matched']
 
                     for eachline in f_in.readlines():
-                        value_list = eachline.strip().split('#')
-                        key_value = tuple(zip(key_list, value_list))
-                        data_list.append(dict(key_value))
+                        valueList = eachline.strip().split('#')
+                        key_value = tuple(zip(keyList, valueList))
+                        dataList.append(dict(key_value))
 
             else:
                 raise Exception('No such model documents provided.'
                                 'Please reselect and try again.')
-            return data_list
+            return dataList
 
         except Exception as e:
             print(e)
@@ -87,13 +89,13 @@ class Model_trans(Mongo):
 
 
 if __name__ == '__main__':
-    fname = 'synonym.txt'
+    fname = 'render_api.txt'
     db_name = 'bookstore' # set datebase name
     ip = '10.89.100.12'
     port = 27017
 
-    model_category = 'synonym'
-    collection = 'synonym' # set collection branch name
+    model_category = 'render_api'
+    collection = 'render_api' # set collection branch name
 
     mt = Model_trans(fname = fname, db_name = db_name ,ip = ip ,port = port , model_category = model_category)
     mt.delete(collation={}, collection = collection)
